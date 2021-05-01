bb88e7d42c804043000e79e39a9ec899aca4ef4a
nghochi123 <35862661+nghochi123@users.noreply.github.com>
2021-05-02 01:05:59 +0800

    hi

--- src/components/medium/BackDrop/BackDropText.js
+++ src/components/medium/BackDrop/BackDropText.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import Tags from '../../small/Tags/Tags';
 import * as styles from './BackDropText.module.css';
 
 export default function BackDropText({ data }) {
+    console.log(data);
     return (
         <div className={styles.backdrop}>
             <img src={data.coverImage.large} alt="image" className={styles.coverimage} />
             <div className={styles.text}>
                 <Typography variant="h2">{data.title.romaji}</Typography>
                 <Typography variant="body1">{data.season} {data.seasonYear}</Typography>
-                <Typography variant="body1">{data.type}</Typography>
-                <Typography variant="body1">{data.format}</Typography>
-                <Typography variant="body1">{data.status}</Typography>
-                <Typography variant="body1">{data.episodes}</Typography>
-                <Typography variant="body1">{data.duration}</Typography>
-                <Typography variant="body1">{data.isAdult}</Typography>
-                <Typography variant="body1">{data.source}</Typography>
+                <Typography variant="body1">Episodes: {data.episodes}</Typography>
+                {data.tags.map(tag=>(
+                    <Tags>{tag.name}</Tags>
+                ))}
 
             </div>
 
