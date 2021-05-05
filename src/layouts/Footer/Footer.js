50a2a301e1f98ff0bc48f24cdaeb20779aafb8aa
nghochi123 <nghochi123@gmail.com>
2021-05-05 18:08:31 +0800

    Forgot the footer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import {Grid} from '@material-ui/core';
+import React, { useContext } from "react";
+import { Grid, IconButton, Typography } from "@material-ui/core";
+import { Language, GitHub, LinkedIn, Code } from "@material-ui/icons";
+import { GlobalStateContext } from "../../context/GlobalContextProvider";
 
 export default function Footer() {
-    return (
-        <Grid container spacing={3} style={{ maxWidth: '100vw', margin: 0, display:'flex', justifyContent: 'center', textAlign:'center' }}>
-            <Grid item xs={6} md={3}>
-                Footer
-            </Grid>
-            <Grid item xs={6} md={3}>
-                Footer
-            </Grid>
-        </Grid>
-    )
+  const darkMode = useContext(GlobalStateContext).darkMode;
+  return (
+    <Grid
+      container
+      spacing={3}
+      style={{
+        maxWidth: "100vw",
+        padding: 50,
+        marginTop: 25,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        backgroundColor: darkMode ? "#111" : "#eee",
+      }}
+    >
+      <Grid item xs={6} md={3}>
+        <Typography variant="body2">AniBoard © 2021</Typography>
+        <Typography variant="body2">Design and code by Ho Chi</Typography>
+      </Grid>
+      <Grid item xs={6} md={3}>
+        <img
+          src="/Anilist.png"
+          width="50"
+          height="50"
+          alt="AniList"
+          onClick={() => {
+            window.open("https://github.com/AniList/ApiV2-GraphQL-Docs");
+          }}
+          style={{ cursor: "pointer" }}
+        />
+        <Typography variant="body2">Built with AniList API</Typography>
+        <IconButton onClick={()=>{window.open('https://nghochi.netlify.app/')}}>
+          <Language />
+        </IconButton>
+        <IconButton onClick={()=>{window.open('https://github.com/nghochi123')}}>
+          <GitHub />
+        </IconButton>
+        <IconButton onClick={()=>{window.open('https://www.linkedin.com/in/ng-ho-chi-809a881b9/')}}>
+          <LinkedIn />
+        </IconButton>
+        <IconButton onClick={()=>{window.open('https://github.com/nghochi123/aniboard')}}>
+          <Code />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
 }
